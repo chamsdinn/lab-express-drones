290e9ac294ab7cd5f9c7178faef64aed0a490d22
chamsdinn <chams86@msn.com>
2022-11-09 19:52:46 +0100

    extra lab done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,29 @@
 // Iteration #1
+require("./../db/index")
+
+const Drone = require("./../models/Drone.model")
+
+const drones = [
+    {
+        name: "Techdrone",
+        propellers: 4,
+        maxSpeed: 25,
+    },
+    {
+        name: "Dronemotion Lite",
+        propellers: 4,
+        maxSpeed: 20,
+    },
+    {
+        name: "Drone XT8",
+        propellers: 4,
+        maxSpeed: 30,
+    }
+]
+
+seed();
+
+async function seed(){
+    await Drone.deleteMany();
+    await Drone.create(drones);
+}
